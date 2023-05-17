9c842f176e1fd143bef63363bf30cf95f5799800
Atendimento2005 <gogo.june2005@gmail.com>
2023-05-17 15:45:46 +0530

    init filesystem

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.app_context().push()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/arimkaji')
+def arimkaji():
+    return render_template('arimkaji.html')
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
