8ce898e172e3e3820d11c6d7c3865b9dc708ef17
Atendimento2005 <gogo.june2005@gmail.com>
2023-05-19 12:04:22 +0530

    sessions_test

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = "filesystem"
+Session(app)
 app.app_context().push()
 
 
@@ -10,27 +14,23 @@ app.app_context().push()
 def index():
     return render_template('homepage.html')
 
-@app.route('/arimkaji')
-def arimkaji():
-    return render_template('arimkaji.html')
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        session['name'] = request.form.get('name')
+        session['email'] = request.form.get('email')
+        return redirect('/home')
+    
+    if session.get('name'):
+        return redirect('/home')
 
-@app.route('/rajukaju')
-def rajukaju():
-    return render_template('rajukaju.html')
+    return render_template('login.html')
 
-@app.route('/riddhi')
-def riddhi():
-    return render_template('riddhi.html')
-
-@app.route('/supro')
-def supro():
-    return render_template('supro.html')
-        
-@app.route('/clasmate')
-def clasmate():
-    return render_template('clasmate.html')
-
-#kaaj korche
+@app.route('/home')
+def home():
+    if not session.get('name'):
+        return redirect('/login')
+    return render_template('home.html', email=session.get('email'))
 
 if __name__ == '__main__':
     app.run(debug=True)
