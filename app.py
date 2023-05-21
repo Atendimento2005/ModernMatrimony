2dbebbf743dda4f7b6d619d95d74ed074ffc24be
Atendimento2005 <gogo.june2005@gmail.com>
2023-05-21 19:17:46 +0530

    fix

--- app.py
+++ app.py
@@ -14,23 +14,5 @@ app.app_context().push()
 def index():
     return render_template('homepage.html')
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    if request.method == 'POST':
-        session['name'] = request.form.get('name')
-        session['email'] = request.form.get('email')
-        return redirect('/home')
-    
-    if session.get('name'):
-        return redirect('/home')
-
-    return render_template('login.html')
-
-@app.route('/home')
-def home():
-    if not session.get('name'):
-        return redirect('/login')
-    return render_template('home.html', email=session.get('email'))
-
 if __name__ == '__main__':
     app.run(debug=True)
